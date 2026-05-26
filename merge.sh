92803acdac1caa2ee4ec69781452220fc499c0e4
BT4DT <149670623+BT4DT@users.noreply.github.com>
2026-05-25 23:34:54 -0700

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -64,6 +64,7 @@ urls=(
 # ===== gambling domain =====
 # === X 🔴 gambling.txt :: DNSFORGE
 #"https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/gambling.txt"
+# =========== END
 )
 
 # ===== download =====
@@ -83,10 +84,16 @@ sort -u cleaned.txt > merged_clean.txt
 cat <<EOF > whitelist.txt
 # ==== WHITELIST ====
 # remove # to enable
-# @@||google.com^
-# @@||gstatic.com^
-# @@||cloudflare.com^
-@@||dnsforge.de^
+#@@||dns.google.com^$important
+#@@||cloudflare.com^$important
+#@@||cloudflare-dns.com^$important
+#@@||gstatic.com^$important
+#@@||github.com^$important
+#@@||tailscale.com^$important
+#@@||zerotier.com^$important
+#@@||goodcloud.xyz^$important
+#@@||astrowarp.net^$important
+#@@||youtubei.googleapis.com^$important
 EOF
 
 # ===== final =====
